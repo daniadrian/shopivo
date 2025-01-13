dac3c945355f381cce963089313d32816913be48
Dani Adrian <daniadrian1311@gmail.com>
2025-01-14 00:12:22 +0700

    dashboard sign-in 4

--- src/app/(admin)/dashboard/(auth)/sign-in/page.tsx
+++ src/app/(admin)/dashboard/(auth)/sign-in/page.tsx
@@ -1,14 +1,4 @@
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import FormSignIn from "./_components/form"
 
 export const description =
   "A simple login form with email and password. The submit button says 'Sign in'."
@@ -17,27 +7,7 @@ export default function SignInPage() {
   return (
     <main className="w-full h-screen overflow-hidden relative">
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <Card className="w-full max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account.
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" required />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full">Sign in</Button>
-      </CardFooter>
-    </Card>
+        <FormSignIn />
         </div>
     </main>
   )
