95d24679f67f1e616a42592e03dda1ee36581ae2
Dani Adrian <daniadrian1311@gmail.com>
2025-01-24 23:59:36 +0700

    Progress Integrasi

---
+++ src/app/(admin)/dashboard/(auth)/sign-in/lib/schema.ts
@@ -0,0 +1,7 @@
+import {z} from 'zod';
+
+export const shcemaSingIn = z.object({
+    email: z.string({required_error: 'Email is required'}).email({message: 'Invalid email'}),
+    password: z.string({required_error: 'Password is required'}).min(8, {message: 'Password must be at least 8 characters long'})
+    
+});
